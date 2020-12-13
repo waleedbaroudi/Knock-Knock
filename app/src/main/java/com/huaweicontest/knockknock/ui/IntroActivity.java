1d1c418ab097b96be0ad561d77f7f07d2e6842f3
waleedbaroudi <waleedbaroudi@hotmail.com>
2020-12-13 03:11:46 +0300

    add viewpager feature, setup basic slide layout, add basis images and texts

--- app/src/main/java/com/huaweicontest/knockknock/ui/IntroActivity.java
+++ app/src/main/java/com/huaweicontest/knockknock/ui/IntroActivity.java
@@ -1,19 +1,34 @@
 package com.huaweicontest.knockknock.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.View;
 
 import com.huaweicontest.knockknock.R;
+import com.huaweicontest.knockknock.ui.fragments.IntroPagerSlide;
 
 public class IntroActivity extends AppCompatActivity {
+
+    private static final int INTRO_PAGE_COUNT = 5;
+    ViewPager2 introPager;
+    IntroSlideAdapter introPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        introPager = findViewById(R.id.intro_pager);
+        introPagerAdapter = new IntroSlideAdapter(this);
+        introPager.setAdapter(introPagerAdapter);
     }
 
     @Override
@@ -22,5 +37,25 @@ public class IntroActivity extends AppCompatActivity {
         //hide status bar
         int uiOption = View.SYSTEM_UI_FLAG_FULLSCREEN;
         getWindow().getDecorView().setSystemUiVisibility(uiOption);
+    }
+
+
+    private class IntroSlideAdapter extends FragmentStateAdapter {
+
+
+        public IntroSlideAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return new IntroPagerSlide(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return INTRO_PAGE_COUNT;
+        }
     }
 }
---
+++ app/src/main/java/com/huaweicontest/knockknock/ui/fragments/IntroPagerSlide.java
@@ -0,0 +1,55 @@
+package com.huaweicontest.knockknock.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.huaweicontest.knockknock.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class IntroPagerSlide extends Fragment {
+    int position;
+
+    TextView introText, introHeading;
+    CircleImageView introFigure;
+
+    String[] headings;
+    String[] texts;
+    int[] imageResources;
+
+    public IntroPagerSlide(int pagerPosition) {
+        position = pagerPosition;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_intro_pager_slide, container, false);
+        introHeading = root.findViewById(R.id.intro_heading);
+        introText = root.findViewById(R.id.intro_text);
+        introFigure = root.findViewById(R.id.intro_figure);
+        try {
+            setViewContents();
+        } catch (Exception ignored) {
+        }
+
+        return root;
+    }
+
+    private void setViewContents() {
+        headings = getResources().getStringArray(R.array.intro_headings);
+        texts = getResources().getStringArray(R.array.intro_texts);
+        imageResources = new int[]{R.drawable.touch, R.drawable.verified, R.drawable.global, R.drawable.molecular};
+        introHeading.setText(headings[position]);
+        introText.setText(texts[position]);
+        introFigure.setImageResource(imageResources[position]);
+    }
+}
