68d8635b8cc60cb6bddb7204bcf99b124ff6e6ce
waleedbaroudi <waleedbaroudi@hotmail.com>
2020-12-13 17:24:30 +0300

    style login activity, add sign out button.

--- app/src/main/java/com/huaweicontest/knockknock/ui/MainActivity.java
+++ app/src/main/java/com/huaweicontest/knockknock/ui/MainActivity.java
@@ -6,10 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.huawei.hms.support.hwid.result.AuthHuaweiId;
 import com.huawei.hms.support.hwid.service.HuaweiIdAuthService;
 import com.huaweicontest.knockknock.R;
@@ -20,6 +24,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class MainActivity extends AppCompatActivity implements AccountHandler.AccountControlListener {
     //Views
     Button signInButton;
+    FloatingActionButton signOutButton;
     TextView nameLabel;
     CircleImageView userImage;
     //Account Control
@@ -32,22 +37,30 @@ public class MainActivity extends AppCompatActivity implements AccountHandler.Ac
         setContentView(R.layout.activity_main);
 
         signInButton = findViewById(R.id.sign_in_button);
+        signOutButton = findViewById(R.id.signout_button);
         nameLabel = findViewById(R.id.name_label);
         userImage = findViewById(R.id.user_image);
 
         handler = new AccountHandler(this, this);
-        signInButton.setOnClickListener(v -> handler.signIn());
+        signInButton.setOnClickListener(v -> {
+            handler.signIn();
+        });
+    }
 
+    private void showSignedInViews() {
+        userImage.setVisibility(View.VISIBLE);
+        nameLabel.setVisibility(View.VISIBLE);
+        signOutButton.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void onSignedIn(AuthHuaweiId userID) {
         nameLabel.setText(userID.getDisplayName());
-        nameLabel.setTextColor(getResources().getColor(R.color.hwid_auth_button_color_blue));
         Uri userImageUri = userID.getAvatarUri();
         if (!TextUtils.isEmpty(userImageUri.toString())) {
             Glide.with(this).load(userImageUri.toString()).into(userImage);
         }
+        showSignedInViews();
     }
 
     @Override
