8699e17d37e6af3bb81af3f98c9f7bde2d7be320
waleedbaroudi <waleedbaroudi@hotmail.com>
2020-12-14 01:45:57 +0300

    add splash screen

--- app/src/main/java/com/huaweicontest/knockknock/model/Constant.java
+++ app/src/main/java/com/huaweicontest/knockknock/model/Constant.java
@@ -6,4 +6,8 @@ public final class Constant {
     public static final int INTRO_PAGE_COUNT = 4;
     public static final String APP_SHARED_PREFS = "M_SHARED_PREFS";
     public static final String FIRST_LAUNCH_BOOL = "FIRST_LAUNCH";
+    public static final int SPLASH_DURATION = 2500;
+    public static final int SPLASH_LOGO_SLIDE_DISTANCE = 150;
+    public static final int SPLASH_SLOGAN_SLIDE_DISTANCE = -30;
+    public static final int SPLASH_ANIM_DURATION = 1200;
 }
---
+++ app/src/main/java/com/huaweicontest/knockknock/ui/SplashActivity.java
@@ -0,0 +1,63 @@
+package com.huaweicontest.knockknock.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.huaweicontest.knockknock.R;
+import com.huaweicontest.knockknock.model.Constant;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView logo;
+    TextView slogan;
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        sharedPreferences = getSharedPreferences(Constant.APP_SHARED_PREFS, MODE_PRIVATE);
+        logo = findViewById(R.id.logo);
+        slogan = findViewById(R.id.splash_slogan);
+        beginAnimations();
+
+        boolean firstLaunch = sharedPreferences.getBoolean(Constant.FIRST_LAUNCH_BOOL, true);
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(Constant.SPLASH_DURATION);
+                if (firstLaunch)
+                    startActivity(new Intent(SplashActivity.this, IntroActivity.class));
+                else
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void beginAnimations() {
+        logo.animate()
+                .translationY(Constant.SPLASH_LOGO_SLIDE_DISTANCE)
+                .setInterpolator(new DecelerateInterpolator())
+                .setDuration(Constant.SPLASH_ANIM_DURATION);
+
+        slogan.animate()
+                .translationY(Constant.SPLASH_SLOGAN_SLIDE_DISTANCE)
+                .setInterpolator(new DecelerateInterpolator())
+                .setDuration(Constant.SPLASH_ANIM_DURATION);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int uiOption = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        getWindow().getDecorView().setSystemUiVisibility(uiOption);
+    }
+}
