c9e5af745e8121b6bbe6cc34e4137b985447cb7c
waleedbaroudi <waleedbaroudi@hotmail.com>
2020-12-13 14:06:37 +0300

    add intro tab indicator, skip button, and override back press behavior

--- app/src/main/java/com/huaweicontest/knockknock/ui/IntroActivity.java
+++ app/src/main/java/com/huaweicontest/knockknock/ui/IntroActivity.java
@@ -4,31 +4,46 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.huaweicontest.knockknock.R;
 import com.huaweicontest.knockknock.ui.fragments.IntroPagerSlide;
 
 public class IntroActivity extends AppCompatActivity {
 
     private static final int INTRO_PAGE_COUNT = 4;
+
     ViewPager2 introPager;
     IntroSlideAdapter introPagerAdapter;
+    TabLayout introIndicator;
+
+    Button skipButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        setupIntroPager();
+        skipButton = findViewById(R.id.skip_intro_button);
+        skipButton.setOnClickListener(v -> startActivity(new Intent(IntroActivity.this, MainActivity.class)));
+    }
+
+    private void setupIntroPager() {
         introPager = findViewById(R.id.intro_pager);
+        introIndicator = findViewById(R.id.intro_indicator);
         introPagerAdapter = new IntroSlideAdapter(this);
         introPager.setAdapter(introPagerAdapter);
+        TabLayoutMediator mediator = new TabLayoutMediator(introIndicator, introPager, (tab, position) -> {
+        });
+        mediator.attach();
     }
 
     @Override
@@ -39,6 +54,14 @@ public class IntroActivity extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility(uiOption);
     }
 
+    @Override
+    public void onBackPressed() {
+        int current = introIndicator.getSelectedTabPosition();
+        if (current == 0) //if at first slide, let the system deal with the back press
+            super.onBackPressed();
+        else //otherwise go to the previous intro slide
+            introPager.setCurrentItem(current - 1, true);
+    }
 
     private class IntroSlideAdapter extends FragmentStateAdapter {
 
