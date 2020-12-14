e709fc4f075a32ddfc9d227625890fe5e2c8cbab
waleedbaroudi <waleedbaroudi@hotmail.com>
2020-12-14 15:23:18 +0300

    fix sign in button behaviour and removed unused drawable

--- app/src/main/java/com/huaweicontest/knockknock/ui/MainActivity.java
+++ app/src/main/java/com/huaweicontest/knockknock/ui/MainActivity.java
@@ -130,7 +130,6 @@ public class MainActivity extends AppCompatActivity implements AccountHandler.Ac
     @Override
     public void onAuthorizationNeeded(Intent signInIntent) {
         Toast.makeText(this, getString(R.string.taost_authorization_needed), Toast.LENGTH_SHORT).show();
-        startActivityForResult(signInIntent, LOGIN_REQUEST_CODE);
     }
 
     @Override
